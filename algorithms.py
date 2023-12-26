b85362aa370f2e15e70b6a06ddf899754dc4ec91
baramundi666 <mkrol@student.agh.edu.pl>
2023-12-26 10:01:11 +0100

    updated documentation, parsed bowyer-watson into algorithms file, updated main

--- algorithms.py
+++ algorithms.py
@@ -1,16 +1,70 @@
+# parametr epsilon
+eps = 10**-12
 def orient(a, b, c):
     return (b.x-a.x)*(c.y-b.y) - (b.y-a.y)*(c.x-b.x)
 
-# parametr epsilon
-eps = 10*-12
+def collinear(x1, y1, x2, y2, x3, y3):
+    a = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)
+    if abs(a)<eps: return True
+    return False
+
+def findCircumCenter(P, Q, R):
+    ax = P[0]
+    ay = P[1]
+    bx = Q[0]
+    by = Q[1]
+    cx = R[0]
+    cy = R[1]
+    d = 2 * (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by))
+    ux = ((ax * ax + ay * ay) * (by - cy) + (bx * bx + by * by) * (cy - ay) + (cx * cx + cy * cy) * (ay - by)) / d
+    uy = ((ax * ax + ay * ay) * (cx - bx) + (bx * bx + by * by) * (ax - cx) + (cx * cx + cy * cy) * (bx - ax)) / d
+    return (ux, uy)
+
+def checkPosition(P, Q, R, D):
+    center = findCircumCenter(P, Q, R)
+    R = (center[0]-P[0])**2 + (center[1]-P[1])**2
+    dist = (center[0]-D[0])**2 + (center[1]-D[1])**2
+    return dist-R<=eps
+
+def obtuseAngle(triangle, edge):
+    a = edge.A
+    b = edge.B
+    if triangle.a!=a and triangle.a!=b: c=triangle.a
+    if triangle.b!=a and triangle.b!=b: c=triangle.b
+    if triangle.c!=a and triangle.c!=b: c=triangle.c
+    lengthAB = (a.x-b.x)**2 + (a.y-b.y)**2
+    lengthBC = (c.x-b.x)**2 + (c.y-b.y)**2
+    lengthCA = (a.x-c.x)**2 + (a.y-c.y)**2
+    return lengthAB>lengthBC+lengthCA
 
 class Point():
-    def __init__(self,point):
+    def __init__(self,point,index):
+        self.index = index
         self.x = point[0]
         self.y = point[1]
 
     def __eq__(self, other):
-        return abs(self.x-other.x)<eps and abs(self.y-other.y)<eps
+        return self.index==other.index
+    
+    def __hash__(self):
+        return hash(self.index)
+    
+    def toCart(self):
+        return (self.x, self.y)
+    
+class Edge():
+    def __init__(self, PointA, PointB):
+        self.A = PointA
+        self.B = PointB
+
+    def __eq__(self, other):
+        return (self.A==other.A and self.B==other.B) or (self.A==other.B and self.B==other.A)
+    
+    def __hash__(self):
+        return hash(self.A.index+self.B.index)
+    
+    def toCart(self):
+        return ((self.A.x, self.A.y), (self.B.x, self.B.y))
 
 
 class Triangle():
@@ -23,81 +77,118 @@ class Triangle():
     def __eq__(self, other):
         return self.a==other.a and self.b==other.b and self.c==other.c
     
+    def __hash__(self):
+        return hash((self.a, self.b, self.c))
+    
     def containsPoint(self, point):
-        side1 = orient(self.a, self.b, point)
-        side2 = orient(self.b, self.c, point)
-        side3 = orient(self.c, self.a, point)
-        isOnLeft = side1<0 and side2<0 and side3<0
-        isOnRight = side1>0 and side2>0 and side3>0
-        return isOnLeft or isOnRight
+        return self.a==point or self.b==point or self.c==point
     
     def circumcircleContainsPoint(self, point):
-        ax_ = self.a.x-point.x
-        ay_ = self.a.y-point.y
-        bx_ = self.b.x-point.x
-        by_ = self.b.y-point.y
-        cx_ = self.c.x-point.x
-        cy_ = self.c.y-point.y
-        return (
-            (ax_*ax_ + ay_*ay_) * (bx_*cy_-cx_*by_) -
-            (bx_*bx_ + by_*by_) * (ax_*cy_-cx_*ay_) +
-            (cx_*cx_ + cy_*cy_) * (ax_*by_-bx_*ay_)
-        ) > 0
+        P = self.a.toCart()
+        Q = self.b.toCart()
+        R = self.c.toCart()
+        D = point.toCart()
+        return checkPosition(P, Q, R, D)
     
+    def getCircleCenter(self):
+        P = self.a.toCart()
+        Q = self.b.toCart()
+        R = self.c.toCart()
+        return Point(findCircumCenter(P, Q, R),-1)
+
     def sharesEdge(self, edge):
-        x = Point(edge[0])
-        y = Point(edge[1])
+        x = edge.A
+        y = edge.B
         trianglePoints = [self.a, self.b, self.c]
         return x in trianglePoints and y in trianglePoints
-        
+    
 
-
-# https://www.baeldung.com/cs/voronoi-diagram
 def Bowyer_Watson(points):
+    lowerX = points[0][0]
+    lowerY = points[0][1]
+    upperX = points[0][0]
+    upperY = points[0][1]
     pointList = []
-    for point in points:
-        newPoint = Point(point)
+    n = len(points)
+    for i in range(n):
+        newPoint = Point(points[i], i)
         pointList.append(newPoint)
+        if points[i][0]<lowerX: lowerX = points[i][0]
+        if points[i][1]<lowerY: lowerY = points[i][1]
+        if points[i][0]>upperX: upperX = points[i][0]
+        if points[i][1]>upperY: upperY = points[i][1]
 
-    # print("!")
-    # print(pointList)
+
     triangleList = []
-    superA = Point((1000,-1000))
-    superB = Point((-1000,-1000))
-    superC = Point((0,1000))
-    superTriangle = Triangle(superA, superB, superC)
+    # Prostokąt
+    lowerX-=100
+    lowerY-=100
+    upperX+=100
+    upperY+=100
+    superA = Point((lowerX, lowerY), -2)
+    superB = Point((lowerX, upperY), -3)
+    superC = Point((upperX, upperY), -4)
+    superD = Point((upperX, lowerY), -5)
+    # Dwa trójkąty
+    superTriangle1 = Triangle(superA, superB, superC)
+    superTriangle2 = Triangle(superC, superD, superA)
+    triangleList.append(superTriangle1)
+    triangleList.append(superTriangle2)
 
+    # Główna pętla algorytmu
     for point in pointList:
-        edgeList = set()
+        # klucz: krawedz, wartość: zbiór trójkątów
+        edgeList = {}
+
+        # Wyznaczenie trójkątów do usunięcia
         for triangle in triangleList:
             if triangle.circumcircleContainsPoint(point):
                 triangle.isCorrect=False
                 a = triangle.a
                 b = triangle.b
                 c = triangle.c
-                edgeList.add((Point(a), Point(b)))
-                edgeList.add( (Point(b), Point(c)))
-                edgeList.add((Point(c), Point(a)))
+                edgeAB = Edge(a, b)
+                edgeBC = Edge(b, c)
+                edgeCA = Edge(c, a)
+                if edgeAB in edgeList.keys():
+                    edgeList[edgeAB].add(triangle)
+                else:
+                    edgeList[edgeAB] = set()
+                    edgeList[edgeAB].add(triangle)
+                if edgeBC in edgeList.keys():
+                    edgeList[edgeBC].add(triangle)
+                else:
+                    edgeList[edgeBC] = set()
+                    edgeList[edgeBC].add(triangle)
+                if edgeCA in edgeList.keys():
+                    edgeList[edgeCA].add(triangle)
+                else:
+                    edgeList[edgeCA] = set()
+                    edgeList[edgeCA].add(triangle)
 
+        # Usunięcie nieprawidłowych trójkątów
+        newTriangleList = []
         for triangle in triangleList:
-            if triangle.isCorrect==False: triangleList.remove(triangle)
+            if triangle.isCorrect==True: newTriangleList.append(triangle)
+        triangleList = newTriangleList
 
-        # to do
-        #for edge in edgeList:
-            # if edge is shared by any other triangles:
-            #    edgeList.remove(edge)
+        # Usunięcie przecinających się krawędzi
+        newEdgeList = dict(edgeList)
+        for edge in edgeList.keys():
+            if len(edgeList[edge])>1:
+                newEdgeList.pop(edge)
+        edgeList = newEdgeList
 
+        # Utworzenie nowych trójkątów - wypełnienie pustego wielokąta
         for edge in edgeList:
-            triangle = Triangle(edge[0], edge[1], point)
+            triangle = Triangle(edge.A, edge.B, point)
             triangleList.append(triangle)
+
     
-    a = superTriangle.a
-    b = superTriangle.b
-    c = superTriangle.c
+    # Usunięcie trójkątów zawierających sztuczne punkty początkowe
+    finalTriangleList = list(triangleList)
     for triangle in triangleList:
-        if triangle.containsPoint(a) or triangle.containsPoint(b) or triangle.containsPoint(c):
-            triangleList.remove(triangle)
+        if triangle.containsPoint(superA) or triangle.containsPoint(superB) or triangle.containsPoint(superC) or triangle.containsPoint(superD):
+            finalTriangleList.remove(triangle)
 
-    return triangleList
-
-#Bowyer_Watson([(0, 1)])
+    return finalTriangleList    
--- documentation.md
+++ documentation.md
@@ -34,7 +34,8 @@ Program składa się z następujących modułów:
 - testy do zaimplementowanych algorytmów
 
 Wymagania techniczne:
-- zainstalowane narzędzie do wizualizacji ***bitalg***
+- zainstalowana biblioteka ***NumPy***
+- zainstalowana biblioteka do wizualizacji ***bitalg***
 
 # Część użytkownika <a name="user"></a>
 Wszystkie moduły opisane powyżej oprócz algorytmów znajdują się w pliku ***main.ipynb***, a algorytmy są importowane z pliku ***algorithms.py***.
@@ -44,3 +45,18 @@ Funkcje ***draw_tri*** oraz ***draw_voronoi*** służą do wizualizacji odpowied
 Funkcja ***generate_uniform_points*** służy do losowego generowania chmury punktów na płaszczyźnie.
 
 # Sprawozdanie <a name="report"></a>
+
+W ramach projektu zaimplementowaliśmy dwa algorytmy wyznaczające dla chmury punktów w 2D wierzchołki diagramu Voronoi. 
+Umożliwilismy również wizualizację samego diagramu oraz kolejnych kroków algorytmów.
+#### 1. Algorytm Bowyera-Watsona
+Algorytm iteracyjny konstruuje triangulację Delaunaya.
+
+Dla każdego nowo-dodanego punktu znajduje trójkąty, których okręgi opisane zawierają ten punkt.
+Następnie usuwa znalezione trójkąty z obecnej triangulacji i w powstałej w skutego tego "dziurze" tworzy nowe trójkąty poprzez połączenie krawędziami nowo-dodanego punktu z sąsiednimi wierzchołkami powstałego wielokąta.
+
+Na podstawie triangulacji Delaunaya chmury punktów, jesteśmy w stanie wyznaczyć wierzchołki diagramu Voronoi - są to środki okręgów opisanych na każdym z trójkątów wyznaczonej triangulacji.
+
+Graficzne przedstawienie diagramu Voroni polega na połączeniu ze sobą odcinkami środków okręgów sąsiadujących ze sobą trójkątów oraz poprowadzenie dla pozostałych krawędzi półprostych wychodzących z wierzchołka Voronoi i pokrywających symetralną obecnie rozważanej krawędzi.
+
+![example1](example1.png)
+
